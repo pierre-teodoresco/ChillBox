d980b564c11b51e73f4451b8a2964ca668721fc7
pierre-teodoresco <p.teodoresco@gmail.com>
2024-10-30 20:12:27 -0400

    adjust success threshold after playtesting

--- app/src/main/java/com/example/chillbox/ui/colorpicker/ColorPickerViewModel.kt
+++ app/src/main/java/com/example/chillbox/ui/colorpicker/ColorPickerViewModel.kt
@@ -3,6 +3,7 @@ package com.example.chillbox.ui.colorpicker
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlin.random.Random
 
 class ColorPickerViewModel : ViewModel() {
@@ -10,7 +11,7 @@ class ColorPickerViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(ColorPickerUiState())
     val uiState: StateFlow<ColorPickerUiState> = _uiState
 
-    private val successThreshold = 25 // 10% threshold, tuneable as needed
+    private val successThreshold = 255*3 / 15 // 15% threshold, tuneable as needed
 
     init {
         generateNewColor()
@@ -18,11 +19,17 @@ class ColorPickerViewModel : ViewModel() {
 
     fun generateNewColor() {
         val targetColor = Triple(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
-        _uiState.value = _uiState.value.copy(targetColor = targetColor, isSuccess = false)
+        _uiState.update {
+            it.copy(
+                targetColor = targetColor,
+                isSuccess = false,
+                userColor = Triple(0, 0, 0)
+            )
+        }
     }
 
     fun updateUserColor(red: Int, green: Int, blue: Int) {
-        _uiState.value = _uiState.value.copy(userColor = Triple(red, green, blue))
+        _uiState.update { it.copy(userColor = Triple(red, green, blue)) }
         checkForSuccess()
     }
 
@@ -34,12 +41,10 @@ class ColorPickerViewModel : ViewModel() {
         val greenDiff = kotlin.math.abs(targetGreen - userGreen)
         val blueDiff = kotlin.math.abs(targetBlue - userBlue)
 
-        val isSuccess = redDiff <= successThreshold &&
-                greenDiff <= successThreshold &&
-                blueDiff <= successThreshold
+        val isSuccess = redDiff + greenDiff + blueDiff <= successThreshold
 
         if (isSuccess) {
-            _uiState.value = _uiState.value.copy(isSuccess = true)
+            _uiState.update { it.copy(isSuccess = true) }
         }
     }
 }
